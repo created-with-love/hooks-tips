beb057102ebead5c0cac2a9f3d54b956db39a789
Vladymyr Ivanchenko <vladymyr.ivanchenko@oracle.com>
2022-08-04 22:15:59 +0300

    first commit

---
+++ src/person-editor/PersonEditor.tsx
@@ -0,0 +1,87 @@
+import localforage from "localforage"
+import React, { ReactElement, useState, useEffect } from "react"
+
+import { LabeledInput, Loading } from "../components"
+import { Person } from "../types/person"
+import { initialPerson } from "../utils"
+
+const savePerson = (person: Person | null): void => {
+  console.log("🚀 person", person)
+  localforage.setItem("person", person)
+}
+
+export function PersonEditor(): ReactElement {
+  const [person, setPerson] = useState<Person | null>(null)
+
+  useEffect(() => {
+    const getPerson = async () => {
+      const person = await localforage.getItem<Person>("person")
+      setPerson(person ?? initialPerson)
+    }
+
+    getPerson()
+  }, [])
+
+  useEffect(() => {
+    savePerson(person)
+  }, [person])
+
+  if (!person) {
+    return <Loading />
+  }
+
+  return (
+    <form
+      className="person-editor"
+      onSubmit={(e) => {
+        e.preventDefault()
+        alert(`Submitting\n${JSON.stringify(person, null, 2)}`)
+      }}
+    >
+      <h2>Person Editor</h2>
+      <LabeledInput
+        label="Firstname:"
+        value={person.firstname}
+        onChange={(e) => {
+          setPerson((state) => ({ ...state!, firstname: e.target.value }))
+        }}
+      />
+      <LabeledInput
+        label="Surname:"
+        value={person.surname}
+        onChange={(e) => {
+          setPerson((state) => ({ ...state!, surname: e.target.value }))
+        }}
+      />
+      <LabeledInput
+        label="Email:"
+        value={person.email}
+        onChange={(e) => {
+          setPerson((state) => ({ ...state!, email: e.target.value }))
+        }}
+      />
+      <LabeledInput
+        label="Address:"
+        value={person.address}
+        onChange={(e) => {
+          setPerson((state) => ({ ...state!, address: e.target.value }))
+        }}
+      />
+      <LabeledInput
+        label="Phone:"
+        value={person.phone}
+        onChange={(e) => {
+          setPerson((state) => ({ ...state!, phone: e.target.value }))
+        }}
+      />
+      <hr />
+      <div className="btn-group">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <hr />
+      <pre>{JSON.stringify(person, null, 2)}</pre>
+    </form>
+  )
+}
---
+++ src/utils/Theme.tsx
@@ -0,0 +1,49 @@
+import localforage from "localforage"
+import React, {
+  createContext,
+  CSSProperties,
+  ReactElement,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react"
+
+export interface ThemeContext {
+  style?: CSSProperties
+  setStyle: (style?: CSSProperties) => void
+}
+
+export const themeContext = createContext<ThemeContext>({
+  setStyle: () => void null,
+})
+
+interface Props {
+  children: ReactNode
+}
+
+const saveTheme = (style: CSSProperties | undefined): void => {
+  localforage.setItem("style", style)
+}
+
+export function ThemeProvider({ children }: Props): ReactElement {
+  const [style, setStyle] = useState<CSSProperties>()
+
+  useEffect(() => {
+    const getPerson = async () => {
+      const stylesFromLocal = await localforage.getItem<CSSProperties>("style")
+      setStyle(stylesFromLocal ?? undefined)
+    }
+
+    getPerson()
+  }, [])
+
+  useEffect(() => {
+    saveTheme(style)
+  }, [style])
+
+  return (
+    <themeContext.Provider value={{ style, setStyle }}>
+      {children}
+    </themeContext.Provider>
+  )
+}
